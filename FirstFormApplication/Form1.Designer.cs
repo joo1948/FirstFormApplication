2ab6618cf0e515759b2cdfec2a703e89bdcf5cc3
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-19 09:39:53 +0900

    초기 커밋.

---
+++ FirstFormApplication/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FirstFormApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myButton
+            // 
+            this.myButton.Location = new System.Drawing.Point(12, 12);
+            this.myButton.Name = "myButton";
+            this.myButton.Size = new System.Drawing.Size(87, 36);
+            this.myButton.TabIndex = 0;
+            this.myButton.Text = "강주영";
+            this.myButton.UseVisualStyleBackColor = true;
+            this.myButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myButton);//버튼을 화면에 출력해주는 것.
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button myButton;
+    }
+}
+
---
+++ FirstFormApplication/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstFormApplication
+{
+    public partial class Form1 : Form
+        //partial calss=> 디자인.cs와 코드.cs를 하나로 만들어 주는 것.-> 두개를 하나의 클래스로 만들어주는 것
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            myButton.Enabled = true;
+            myButton.Text = "안녕하세요";
+            myButton.Width = 200;
+
+            for(int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                button.Location = new Point(213, 13 + (23 + 3)*i);
+                Controls.Add(button);
+                button.Text = "동적생성"+i+"번째";
+                button.Width = 100;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
